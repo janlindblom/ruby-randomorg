a272461278720912cc826fbcce584486622162c9
Jan Lindblom <janlindblom@fastmail.fm>
2019-11-25 00:05:30 +0200

    Tinkering.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 require 'simplecov'
-require 'simplecov-rcov'
-SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+require 'simplecov_small_badge'
+
+formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCovSmallBadge::Formatter
+]
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(formatters)
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
